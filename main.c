0a7df34c874c60091f4a5d7df6d11f9e1d31967a
wughangar <wughangar.rose@gmail.com>
2023-05-25 09:20:02 +0000

    spellings corrected

--- main.c
+++ main.c
@@ -74,11 +74,11 @@ int main(int argc, char *argv[])
 		}
 		else if (strcmp(opcode, "add") == 0)
 		{
-			add(&stack);
+			add(&stack, line_number);
 		}
 		else if (strcmp(opcode, "nop") == 0)
 		{
-			nop(&stack);
+			nop(&stack, line_number);
 		}
 		else
 		{
